0475828c147753254f27b1480d8bb42d2a67fde8
User <nicitakormakov@gmail.com>
2020-11-27 21:31:55 +0500

    V2

--- Project11/Project11/Source.cpp
+++ Project11/Project11/Source.cpp
@@ -16,38 +16,41 @@ int Row(ifstream &in)
 	return Count_str_;
 }
 
-double MaxLenght(double** Cords_, int Count_str_) 
+template<typename T>
+void MaxLenght(T** Cords_, int Count_str_) 
 {
-	double max = 0;
+	T max = 0;
 	int chek = 0;
-	double tmp = 0;
+	T tmp = 0;
 	for (int i = 0; i < Count_str_; i++)
 	{
 		tmp = sqrt(pow(Cords_[i][0], 2) + pow(Cords_[i][1], 2) + pow(Cords_[i][2], 2));
-		if ((tmp-max)>=0.0000001) 
+		if (abs(tmp-max)>=0.00000001) 
 		{
 			chek = i;
 			max = tmp;
 		}
 	}
-	return max;
+	cout << "Максимальное расстояние от начала координат имеет следущая точка:\n";
+	cout << "X = " << Cords_[chek][0] << " Y = " << Cords_[chek][1] << " Z = " << Cords_[chek][2] << "  d = " << max << endl;
 }
 
 
 int main() 
 {
+	setlocale(LC_ALL, "rus");
 	ifstream in;
-	double** Cords;
+	int** Cords;
 	int Count_cord;
 	double Leng = 0;
 	in.open("input.txt");
 	Count_cord = Row(in);
-	Cords = new double*[Count_cord]; 
+	Cords = new int*[Count_cord]; 
 	in.close();
 	in.open("input.txt");
 	for (size_t i = 0; i < Count_cord; i++)
 	{
-		Cords[i] = new double[Count_cord];
+		Cords[i] = new int[Count_cord];
 	}
 	for (size_t i = 0; i < Count_cord; i++)
 	{
@@ -57,11 +60,7 @@ int main()
 		}
 	}
 
-	Leng = MaxLenght(Cords, Count_cord);
-	cout << Leng << endl;
-
-
-
+	MaxLenght(Cords, Count_cord);
 
 	system("pause");
 	return 0;
